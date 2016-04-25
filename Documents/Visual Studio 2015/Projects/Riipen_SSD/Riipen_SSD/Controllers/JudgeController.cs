a63e057389bcb8178f599daef50c05025779f5e4
whatthefelix <felixisfeng@gmail.com>
2016-04-25 10:07:11 -0700

    Initial Commit

---
+++ Documents/Visual Studio 2015/Projects/Riipen_SSD/Riipen_SSD/Controllers/JudgeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Riipen_SSD.Controllers
+{
+    public class JudgeController : Controller
+    {
+        // GET: Judge
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Documents/Visual Studio 2015/Projects/Riipen_SSD/Riipen_SSD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Riipen_SSD.Startup))]
+namespace Riipen_SSD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
