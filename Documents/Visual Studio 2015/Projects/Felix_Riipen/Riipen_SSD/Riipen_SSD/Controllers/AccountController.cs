471a39f7fdb6a14869b90349d3889d945d927f8f
whatthefelix <felixisfeng@gmail.com>
2016-04-29 11:02:05 -0700

    Login redirect working

--- Documents/Visual Studio 2015/Projects/Felix_Riipen/Riipen_SSD/Riipen_SSD/Controllers/AccountController.cs
+++ Documents/Visual Studio 2015/Projects/Felix_Riipen/Riipen_SSD/Riipen_SSD/Controllers/AccountController.cs
@@ -49,20 +49,24 @@ namespace Riipen_SSD.Controllers
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var user = await UserManager.FindAsync(model.Email, model.Password);
+            var roles = await UserManager.GetRolesAsync(user.Id);
             switch (result)
             {
                 case SignInStatus.Success:
-                    //if (Roles.IsUserInRole(model.Email, "Admin"))
-                    //{
-                    //    return RedirectToAction("Index", "Admin");
-                    //}else if (User.IsInRole("Judge"))
-                    //{
-                    //    return RedirectToAction("Index", "Judge");
-                    //}else
-                    //{
-                    //    return RedirectToAction("Index", "Participant");
-                    //}
-                    return RedirectToAction("Index", "Participant");
+                    if (roles.Contains("Admin"))
+                    {
+                        return RedirectToAction("Index", "Admin");
+                    }
+                    else if (roles.Contains("Judge"))
+                    {
+                        return RedirectToAction("Index", "Judge");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Participant");
+                    }
+               
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
