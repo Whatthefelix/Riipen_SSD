466b22f26a9bcb0ab500325d64aee6cf61102655
whatthefelix <felixisfeng@gmail.com>
2016-05-02 12:11:15 -0700

    ActionLink to ViewScores from contest Details works, made tables responsive and added search/sort to the admin view

--- Documents/Visual Studio 2015/Projects/Felix_Riipen/Riipen_SSD/Riipen_SSD/Controllers/AccountController.cs
+++ Documents/Visual Studio 2015/Projects/Felix_Riipen/Riipen_SSD/Riipen_SSD/Controllers/AccountController.cs
@@ -49,11 +49,13 @@ namespace Riipen_SSD.Controllers
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
-            var user = await UserManager.FindAsync(model.Email, model.Password);
-            var roles = await UserManager.GetRolesAsync(user.Id);
             switch (result)
             {
+
+
                 case SignInStatus.Success:
+                    var user = await UserManager.FindAsync(model.Email, model.Password);
+                    var roles = await UserManager.GetRolesAsync(user.Id);
                     if (roles.Contains("Admin"))
                     {
                         return RedirectToAction("Index", "Admin");
