b09c009b70e03ecea0f2b0595c777bf22a2cfa97
zaichaopan <zaichaopan@gmail.com>
2016-05-02 15:04:33 -0700

    fix slowing score in judge/team

--- Documents/Visual Studio 2015/Projects/Felix_Riipen/Riipen_SSD/Riipen_SSD/JavaScript/Judge_Team_form.js
+++ Documents/Visual Studio 2015/Projects/Felix_Riipen/Riipen_SSD/Riipen_SSD/JavaScript/Judge_Team_form.js
@@ -1,29 +1,33 @@
-﻿$(function () {
+﻿function getScore() {
     var allScores = $('.radioScore');
     var length = allScores.length;
+    var sum = 0;
+    var checkNum = 0;
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        if ($(allScores[i]).is(':checked')) {
+            sum = parseInt(sum) + parseInt($(allScores[i]).val());
+            checkNum++;
+        }
+    }
+
+    if (sum!=0) {
+        sum = sum / checkNum;
+        console.log(sum);
+        sum = sum.toString() + "/7"
+        ; $(".yourScore").html(sum);
+        console.log("hi");
+    }
    
 
+}
+
+$(function () {
+    getScore();
     $('.radioScore').click(function () {
-        var sum = 0;
-        var checkNum = 0;
-       
-        for (var i = 0; i < allScores.length; i++) {
-            
-            if ($(allScores[i]).is(':checked')) {                
-                sum = parseInt(sum) + parseInt($(allScores[i]).val());
-                checkNum++;
-            }          
-        }
-       
-        sum = sum / checkNum;
-        sum = sum.toString() + "/7"
-        ;        $(".yourScore").html(sum);
-        
+        getScore();        
     });
 
- 
-
-   
-   
 
 });
